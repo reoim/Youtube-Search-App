56dd8c2d35a84187e127952aaf88604dad77a93e
itintern <itintern>
2016-12-07 15:49:21 +1100

    Ajax request with React

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
+
   // const video = props.video;
-  // console.log(video);
+  // const onVideoSelect = props.onVideoSelect;
+
 
   const imageUrl = video.snippet.thumbnails.default.url;
   return (
-    <li className="list-group-item">
+    <li onClick={function(){return onVideoSelect(video);}} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
          <img className="media-object" src={imageUrl}/>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyD0rhp3sn1T29EY5jk0Gv1N_AhhMFH7FjY';
 
 // Create a new component. this component should prodce
@@ -12,21 +13,30 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
 
 		YTSearch({key: API_KEY, term: 'dopa twisted fate'}, (videos) => {
-			this.setState({ videos });
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			 });
 			// in ES6, if key and value are same you can use ({ value })
 			// this.setState({ videos: videos })
 		});
 	}
 
 
-	render(){
+	render() {
 		return (
 	    <div>
 	      <SearchBar />
-				<VideoList videos={this.state.videos} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
 	    </div>
 	  );
 	}
